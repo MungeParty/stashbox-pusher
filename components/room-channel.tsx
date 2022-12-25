5a95989bae29a6808e6d554f236372ee56163be8
Thomas Buscaglia <thomas@rizegames.net>
2022-12-24 21:36:48 -0800

    ...

--- components/room-channel.tsx
+++ components/room-channel.tsx
@@ -13,28 +13,31 @@ const chatFetchConfig: any = {
 // bot reply delay
 const botReplyDelay = 200;
 const prompts = [
+  `I got 99 _ and a _ ain't _`,
+  `You _ all the _ you don't _.`,
+  `_? Damn near _!`,
+  `_ or get off the _`,
+  `I'm _ and I'm _`,
   `Gimmme that _! That's not your _!`,
-  `If it's _ you're lookin' for, Andy's not you're _.`,
-  `_, _, _.`,
+  `If you _ you _. `,
+  `East coast girls are _.`,
+  
 
   `Surf's _!`,
   `Please _ my _.`,
   `All _ and no _ makes _ a _ _.`,
-  
+
   `And I would know! I'm _ of _.`,
-  `_ or get off the _`,
   `There is no _ - only _!`,
   `_ lobster. Rock the _`,
   `I climbed to the top of the _ and saw _`,
   `_?! That's no _!`,
-  `You _ all the _ you don't _.`,
   `Hang _.`,
-  `East coast girls are _.`,
+  
   `_-adena! _ City!`,
   `You _.. and then I'll _.`,
   `If we _ , they win.`,
   `If you _ it they will _...`,
-  `I got 00 _ and a _ ain't _`,
   `What's _ got to do with it?`,
   `_ your _ ya filthty animal.`,
   `Ooops I _ again..`,
@@ -48,6 +51,11 @@ const prompts = [
   `If I can't _ then what has this all been about?`,
   `You know, you'd catch more _ with _.`,
   `It's fine. It's _, but it's fine.`,
+  `If it's _ you're lookin' for, Andy's not you're _.`,
+  
+  `_, _, _.`,
+  
+  
   `We always _ what we can't _.`,
   `Keep _ and carry _.`,
   `Like a good _, _ is there!`,
